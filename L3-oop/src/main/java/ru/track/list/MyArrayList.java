11308ee8048d0fbaba05623fd31a5f1fcb9ed5b3
Darayavaus <bor.astat@gmail.com>
2018-03-08 02:17:34 +0300

    task list

--- L3-oop/src/main/java/ru/track/list/MyArrayList.java
+++ L3-oop/src/main/java/ru/track/list/MyArrayList.java
@@ -11,31 +11,51 @@ import java.util.NoSuchElementException;
  */
 public class MyArrayList extends List {
 
-    public MyArrayList() {
+    int[] elementData;
+    int size;
 
+    public MyArrayList() {
+        this(10);
     }
 
     public MyArrayList(int capacity) {
-
+        this.elementData = new int[capacity];
     }
 
     @Override
     void add(int item) {
-
+        if (this.size < this.elementData.length) {
+            this.elementData[this.size++] = item;
+        } else {
+            int[] newArray = new int[this.elementData.length+1];
+            System.arraycopy(this.elementData, 0, newArray, 0, this.elementData.length);
+            newArray[newArray.length-1] = item;
+            this.elementData = newArray;
+            this.size++;
+        }
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
-        return 0;
+        if (idx<0 | idx >= this.size) throw new NoSuchElementException();
+        int[] newArray = new int[this.elementData.length-1];
+        System.arraycopy(this.elementData, 0, newArray, 0, idx);
+        System.arraycopy(this.elementData, idx+1, newArray, idx, this.size-idx-1);
+        int result = this.get(idx);
+        this.elementData = newArray;
+        this.size--;
+        return result;
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        if (idx<0 | idx >= this.size) throw new NoSuchElementException();
+        return this.elementData[idx];
     }
 
     @Override
     int size() {
-        return 0;
+        return this.size;
     }
 }
+
--- L3-oop/src/main/java/ru/track/list/MyLinkedList.java
+++ L3-oop/src/main/java/ru/track/list/MyLinkedList.java
@@ -7,13 +7,16 @@ import java.util.NoSuchElementException;
  * Односвязный список
  */
 public class MyLinkedList extends List {
-
     /**
      * private - используется для сокрытия этого класса от других.
      * Класс доступен только изнутри того, где он объявлен
      * <p>
      * static - позволяет использовать Node без создания экземпляра внешнего класса
      */
+
+    int size;
+    Node header;
+
     private static class Node {
         Node prev;
         Node next;
@@ -26,22 +29,49 @@ public class MyLinkedList extends List {
         }
     }
 
+    public MyLinkedList() {
+        this.header = new Node(null, null, 0);
+        this.header.next = header;
+        this.header.prev = header;
+    }
+
     @Override
     void add(int item) {
+        Node newNode = new Node(this.header.prev, this.header, item);
+        newNode.prev.next = newNode;
+        newNode.next.prev = newNode;
+        this.size++;
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
-        return 0;
+        if (idx<0 | idx>=this.size) throw new NoSuchElementException();
+        Node itNode = header.next;
+        int i = 0;
+        while (i<idx) {
+            itNode = itNode.next;
+            i++;
+        }
+        itNode.prev.next = itNode.next;
+        itNode.next.prev =  itNode.prev;
+        this.size--;
+        return itNode.val;
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        if (idx<0 | idx>=this.size) throw new NoSuchElementException();
+        Node itNode = header.next;
+        int i = 0;
+        while (i<idx) {
+            itNode = itNode.next;
+            i++;
+        }
+        return itNode.val;
     }
 
     @Override
     int size() {
-        return 0;
+        return this.size;
     }
 }
