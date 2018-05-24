4d10d1ade2cd67dd04d44906f8e6f149a15e3685
Darayavaus <bor.astat@gmail.com>
2018-05-25 01:34:43 +0300

    threads

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,11 +1,26 @@
 package ru.track.prefork;
 
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.concurrent.Callable;
+
+
+class Message implements Serializable{
+    public String data;
+
+    public Message(String data) {
+        this.data = data;
+    }
+
+    public String get_data(){
+        return data;
+    }
+}
+
 
 public class Client {
     static Logger log = LoggerFactory.getLogger(Client.class);
@@ -20,21 +35,148 @@ public class Client {
 
     public void loop() throws Exception {
         Socket socket = new Socket(host, port);
-        final OutputStream out = socket.getOutputStream();
-        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            String line = scanner.nextLine();
-            if ("q".equals(line)) {
-                break;
-            }
-            out.write(line.getBytes());
-            out.flush();
-        }
+        Writer writer = new Writer(socket);
+        writer.start();
+
+        Reader reader = new Reader(socket);
+        reader.start();
     }
 
     public static void main(String[] args) throws Exception {
         Client client = new Client(9000, "localhost");
         client.loop();
+    }
+
+
+    public class Writer extends Thread{
+//        Scanner scanner;
+        BufferedReader br;
+        ObjectOutputStream oos;
+        OutputStream out;
+        Socket socket;
+
+        public Writer(Socket socket){
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+            this.socket = socket;
+            this.setName("writer");
+            try {
+                this.out = this.socket.getOutputStream();
+                this.oos = new ObjectOutputStream(this.out);
+            } catch (IOException e1) {
+                System.out.println("Smth wrong with Input/Output e0");
+            }
+//            this.scanner = new Scanner(System.in);
+            this.oos = oos;
+        }
+
+        @Override
+        public void run(){
+            try {
+                while (!isInterrupted()) {
+                    while (!br.ready()) {
+                        Thread.sleep(200);
+                    }
+                    String line = br.readLine();
+                    System.out.println("1");
+                    Message msg = new Message(line);
+                    System.out.println("2");
+                    this.oos.writeObject(msg);
+//                    this.oos.flush();
+                    if ("q".equals(line)) {
+                        this.interrupt();
+                    }
+                }
+            } catch (InterruptedException e0) {
+                System.out.println("Writer was interrupted");
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+
+            try {
+                this.oos.close();
+                System.out.println("closed output stream");
+                this.socket.close();
+                System.out.println("closed socket");
+                ThreadGroup currentGroup = Thread.currentThread().getThreadGroup();
+                int noThreads = currentGroup.activeCount();
+                Thread[] lstThreads = new Thread[noThreads];
+                currentGroup.enumerate(lstThreads);
+                for (int i = 0; i < noThreads; i++) {
+                    Thread tmpthread = lstThreads[i];
+                    if (tmpthread instanceof Reader){
+                        tmpthread.interrupt();
+                        ((Reader) tmpthread).ois.close();
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    public class Reader extends Thread{
+
+        ObjectInputStream ois;
+        InputStream inputStream;
+        Socket socket;
+
+        public Reader(Socket socket){
+            this.setName("reader");
+            try{
+                this.socket = socket;
+                this.inputStream = this.socket.getInputStream();
+                this.ois = new ObjectInputStream(this.inputStream);
+            } catch (Exception e){
+
+            }
+        }
+
+        @Override
+        public void run(){
+            while (!isInterrupted()) {
+                try {
+                    Message resp = (Message) ois.readObject();
+                    String responseLine = resp.get_data();
+                    if ("q".equals(responseLine)) {
+                        System.out.println("got q from server");
+                        this.interrupt();
+                    } else {
+                        System.out.println(responseLine);
+                    }
+                } catch (IOException e1) {
+
+                } catch (ClassNotFoundException e2) {
+
+                }
+            }
+            try{
+                this.ois.close();
+                System.out.println("closed input stream");
+                this.socket.close();
+                System.out.println("closed socket");
+                ThreadGroup currentGroup = Thread.currentThread().getThreadGroup();
+                int noThreads = currentGroup.activeCount();
+                Thread[] lstThreads = new Thread[noThreads];
+                currentGroup.enumerate(lstThreads);
+                for (int i = 0; i < noThreads; i++) {
+                    Thread tmpthread = lstThreads[i];
+                    if (tmpthread instanceof Writer){
+                        System.out.println("interrupting writer");
+                        tmpthread.interrupt();
+                        //doing this because scanner locks
+//                        String s = "q";
+//                        InputStream testInput = new ByteArrayInputStream( s.getBytes("UTF-8") );
+//                        System.setIn(testInput);
+//                        ((Writer) tmpthread).oos.close();
+//                        ((Writer) tmpthread).socket.close();
+                    }
+                }
+            }catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
     }
 }
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,12 +1,18 @@
 package ru.track.prefork;
 
-import java.io.InputStream;
+import java.io.*;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.regex.Pattern;
+
+import javax.swing.tree.ExpandVetoException;
 
 
 /**
@@ -17,6 +23,9 @@ import org.slf4j.LoggerFactory;
  * - save threads
  * - broadcast (fail-safe)
  */
+
+
+
 public class Server {
     static Logger log = LoggerFactory.getLogger(Server.class);
 
@@ -26,15 +35,137 @@ public class Server {
     }
 
     public void serve() throws Exception {
+        AdminConsole admin = new AdminConsole();
+        admin.start();
+        int count=0;
         ServerSocket serverSocket = new ServerSocket(port, 10, InetAddress.getByName("localhost"));
         while (true) {
             log.info("on select...");
             final Socket socket = serverSocket.accept();
-            InputStream inputStream = socket.getInputStream();
+            count++;
+            Thread t = new ServeClient(socket, count);
+            System.out.println("Starting thread "+ t.getName());
+            t.start();
+        }
+    }
 
-            byte[] buf = new byte[1024];
-            int nRead = inputStream.read(buf);
-            System.out.println(new String(buf, 0, nRead));
+    public class ServeClient extends Thread{
+        private Socket socket;
+        int ClientId;
+        int port;
+        String address;
+        InputStream inputStream;
+        OutputStream out;
+        ObjectOutputStream oos;
+        ObjectInputStream ois;
+
+        public ServeClient(Socket socket, int count) {
+            this.socket = socket;
+            this.ClientId = count;
+            this.port = this.socket.getPort();
+            this.address = this.socket.getLocalAddress().toString();
+            this.setName(String.format("Client[%d]@%s:%d", this.ClientId, this.address, this.port));
+            try {
+                inputStream = this.socket.getInputStream();
+                out = this.socket.getOutputStream();
+                oos = new ObjectOutputStream(out);
+                ois = new ObjectInputStream(inputStream);
+            } catch (IOException e1) {
+                System.out.println("Smth wrong with Input/Output e0");
+            }
+
+        }
+
+        @Override
+        public void run(){
+            try {
+                while (!isInterrupted()) {
+                    Message msg = (Message) ois.readObject();
+                    String line = msg.get_data();
+                    System.out.println(line);
+                    if (line.equals("q")) {
+                        throw new InterruptedException();
+                    }
+                    msg.data = String.format("Client@%s:%d>", this.address, this.port) + msg.get_data();
+                    ThreadGroup currentGroup = Thread.currentThread().getThreadGroup();
+                    int noThreads = currentGroup.activeCount();
+                    Thread[] lstThreads = new Thread[noThreads];
+                    currentGroup.enumerate(lstThreads);
+                    for (int i = 0; i < noThreads; i++) {
+                        Thread tmpthread = lstThreads[i];
+                        if (tmpthread instanceof ServeClient && ((ServeClient) tmpthread).ClientId != this.ClientId) {
+                            ((ServeClient) tmpthread).oos.writeObject(msg);
+                            ((ServeClient) tmpthread).oos.flush();
+                        }
+                    }
+
+                }
+            } catch (Exception E) {
+                if (E instanceof InterruptedException){
+                    System.out.println("Client initiated closing connection " + this.getName());
+                }
+                else {
+                    System.out.println(E);
+//                    System.out.println("Some other mistake");
+                }
+                try {
+                    ois.close();
+                    oos.close();
+                    socket.close();
+                } catch (IOException e2){
+                    System.out.println("Smth wrong with Input/Output e2");
+                }
+                System.out.println("Connection closed " + this.getName());
+            }
+        }
+    }
+
+    public class AdminConsole extends Thread{
+        Scanner scanner;
+        public AdminConsole(){
+            this.scanner = new Scanner(System.in);
+        }
+        public void run(){
+            while(true){
+                String line = scanner.nextLine();
+                if ("list".equals(line.toLowerCase())){
+                    ThreadGroup currentGroup = Thread.currentThread().getThreadGroup();
+                    int noThreads = currentGroup.activeCount();
+                    Thread[] lstThreads = new Thread[noThreads];
+                    currentGroup.enumerate(lstThreads);
+                    for (int i = 0; i < noThreads; i++) {
+                        Thread tmpthread = lstThreads[i];
+                        if (tmpthread instanceof ServeClient) {
+                            System.out.println(tmpthread.getName());
+                        }
+                    }
+                }
+                if (Pattern.matches("^drop ++[0-9]++$", line.toLowerCase())){
+                    String delims = " ";
+                    String[] tokens = line.split(delims);
+                    int killId = Integer.parseInt(tokens[tokens.length-1]);;
+                    ThreadGroup currentGroup = Thread.currentThread().getThreadGroup();
+                    int noThreads = currentGroup.activeCount();
+                    Thread[] lstThreads = new Thread[noThreads];
+                    currentGroup.enumerate(lstThreads);
+                    for (int i = 0; i < noThreads; i++) {
+                        Thread tmpthread = lstThreads[i];
+                        if (tmpthread instanceof ServeClient && ((ServeClient) tmpthread).ClientId == killId){
+                            System.out.println("Closing connection "+ tmpthread.getName());
+                            tmpthread.interrupt();
+                            try{
+                                //doing this because readobject() method doesn't throw InterruptedException
+                                //trying to make it throw Exception with streams instead
+                                ((ServeClient) tmpthread).oos.writeObject(new Message("q"));
+                                ((ServeClient) tmpthread).ois.close();
+                                ((ServeClient) tmpthread).oos.close();
+                            } catch (IOException e){
+
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
